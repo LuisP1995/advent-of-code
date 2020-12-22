fc87e275d1a06d3a0893019e2cb392d0a16f8ed6
Luis P <luisp@sire.dstl.gov.uk>
2020-12-22 00:51:50 +0000

    adding skeleton for implementation

---
+++ day2/day2_test.go
@@ -0,0 +1,19 @@
+package day2
+
+import "testing"
+
+struct Passwords {
+	
+}
+
+func testPasswordInterface() string []{
+
+}
+
+func TestLoadPassword(t *testing.T) {
+
+}
+
+func TestParser(t *testing.T) {
+
+}
